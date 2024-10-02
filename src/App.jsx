b02f22c4a8904c018072578f262808a52f4a09d1
ahmadibni <ahmadibni007@gmail.com>
2024-10-02 08:24:47 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import headerImage from "./assets/quiz-logo.png"
+
+function App() {
+  return (
+    <header>
+      <img src={headerImage} alt="" />
+      <h1></h1>
+    </header>
+  );
+}
+
+export default App;
